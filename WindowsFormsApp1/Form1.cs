940fa8c8858f757e04f962694d995200a5c4d03d
Connor Harris <rc0033114@student.riversidecollege.ac.uk>
2018-11-19 15:24:21 +0000

    said hello

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -24,7 +24,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            MessageBox.Show("hello"); 
         }
     }
 }
