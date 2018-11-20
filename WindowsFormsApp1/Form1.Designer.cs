58123c0e47d58757f3e79c93dd7127793f0d5e34
Connor Harris <rc0033114@student.riversidecollege.ac.uk>
2018-11-20 09:45:58 +0000

    SAY HELLO

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.tXTBXNAME = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,22 +42,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tXTBXNAME
+            // 
+            this.tXTBXNAME.Location = new System.Drawing.Point(304, 38);
+            this.tXTBXNAME.Name = "tXTBXNAME";
+            this.tXTBXNAME.Size = new System.Drawing.Size(100, 20);
+            this.tXTBXNAME.TabIndex = 1;
+            this.tXTBXNAME.Text = "BWEY";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tXTBXNAME);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tXTBXNAME;
     }
 }
 
